03befa4e9954fae82163eeec91f61b7c24f4f9cd
毛亚琛 <maoyachen55@gmail.com>
2024-05-20 22:47:13 +0800

    add -D

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-install -v -m 755 node_exporter /usr/local/bin/node_exporter
-install -v -m 644 node_exporter.service /usr/local/lib/systemd/system/node_exporter.service
+install -D -v -m 755 node_exporter /usr/local/bin/node_exporter
+install -D -v -m 644 node_exporter.service /usr/local/lib/systemd/system/node_exporter.service
 if [ ! -f /var/lib/node_exporter/server_info.prom ]; then
     read -p "Please input server name: " server_name
     read -p "Please input group name: " group_name
@@ -17,9 +17,9 @@ systemctl daemon-reload && systemctl enable node_exporter && systemctl restart n
 
 read -p "Whether install Nvidia GPU Exporter? [y/N] " install_nvidia_gpu_exporter
 if [ "$install_nvidia_gpu_exporter" = "y" ]; then
-    install -v -m 755 nvidia_gpu_exporter /usr/local/bin/nvidia_gpu_exporter
+    install -D -v -m 755 nvidia_gpu_exporter /usr/local/bin/nvidia_gpu_exporter
     if [ ! -f /usr/local/lib/systemd/system/nvidia_gpu_exporter.service ]; then
-        install -v -m 644 nvidia_gpu_exporter.service /usr/local/lib/systemd/system/nvidia_gpu_exporter.service
+        install -D -v -m 644 nvidia_gpu_exporter.service /usr/local/lib/systemd/system/nvidia_gpu_exporter.service
     fi
     systemctl daemon-reload && systemctl enable nvidia_gpu_exporter && systemctl restart nvidia_gpu_exporter
 fi
